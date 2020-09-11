01ff39dd4f145855ef5b562fc503465753701786
Jakub Kondrat <jkondrat@soldevelo.com>
2020-09-11 20:32:06 +0200

    benetech/ServiceNet#1129: Improve performance of loading 'owned records'

--- src/main/webapp/app/modules/provider/user-records.tsx
+++ src/main/webapp/app/modules/provider/user-records.tsx
@@ -18,7 +18,17 @@ import ButtonPill from 'app/modules/provider/shared/button-pill';
 
 export interface IUserRecordsProps extends StateProps, DispatchProps {}
 
-export class UserRecords extends React.Component<IUserRecordsProps> {
+const SLIDES_TO_SHOW = 3;
+const PAGE_SIZE = SLIDES_TO_SHOW * 2;
+
+interface IUserRecordsState {
+  index: number;
+}
+
+export class UserRecords extends React.Component<IUserRecordsProps, IUserRecordsState> {
+  state = {
+    index: 0
+  };
   slider: any;
   constructor(props) {
     super(props);
@@ -26,7 +36,7 @@ export class UserRecords extends React.Component<IUserRecordsProps> {
   }
 
   componentDidMount() {
-    this.props.getProviderRecords();
+    this.props.getProviderRecords(0, PAGE_SIZE);
   }
 
   next = () => {
@@ -51,19 +61,38 @@ export class UserRecords extends React.Component<IUserRecordsProps> {
     </div>
   );
 
-  render() {
+  recordCard(index) {
     const { records } = this.props;
+    const record = records[index];
+    return <RecordCard key={index} record={record} link={record ? `record/${record.organization.id}/edit` : ''} />;
+  }
+
+  beforeIndexChange = (oldIndex, newIndex) => {
+    const { records, recordsTotal } = this.props;
+    const firstIdx = newIndex - 1;
+    const missingIndexes = Array.from(Array(SLIDES_TO_SHOW).keys())
+      .map(i => firstIdx + i)
+      .filter(index => index > 0 && index < recordsTotal && !records[index]);
+    const pagesToLoad = _.sortedUniq(missingIndexes.map(index => index / PAGE_SIZE | 0));
+    for (const page of pagesToLoad) {
+      this.props.getProviderRecords(page, PAGE_SIZE);
+    }
+  }
+
+  render() {
+    const { recordsTotal } = this.props;
     const settings = {
       className: 'center',
       dots: true,
       arrows: false,
       speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3,
+      slidesToShow: SLIDES_TO_SHOW,
+      slidesToScroll: SLIDES_TO_SHOW,
       infinite: true,
       initialSlide: 0,
       adaptiveHeight: true,
       appendDots: this.addCustomDots,
+      beforeChange: this.beforeIndexChange,
       responsive: [
         {
           breakpoint: 1024,
@@ -83,7 +112,6 @@ export class UserRecords extends React.Component<IUserRecordsProps> {
         }
       ]
     };
-
     return (
       <Slider ref={c => (this.slider = c)} {...settings}>
         <Card className="record-card new-record mx-3 mb-4">
@@ -100,16 +128,15 @@ export class UserRecords extends React.Component<IUserRecordsProps> {
             </div>
           </CardBody>
         </Card>
-        {_.map(records, record => (
-          <RecordCard key={record.organization.id} record={record} link={`record/${record.organization.id}/edit`} />
-        ))}
+        {_.map(Array.from(Array(recordsTotal).keys()), index => this.recordCard(index))}
       </Slider>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  records: state.providerRecord.records
+  records: state.providerRecord.recordsByIndex,
+  recordsTotal: state.providerRecord.recordsTotal
 });
 
 const mapDispatchToProps = {
