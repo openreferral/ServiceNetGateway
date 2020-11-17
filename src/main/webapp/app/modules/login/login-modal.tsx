12123f6ea2fc2d16cb2124ecf4cc5a70486f53ff
Jakub Kondrat <jkondrat@soldevelo.com>
2020-11-17 16:03:12 +0100

    benetech/ServiceNet#1359: Move the captcha logo behind log-in modal

--- src/main/webapp/app/modules/login/login-modal.tsx
+++ src/main/webapp/app/modules/login/login-modal.tsx
@@ -50,87 +50,90 @@ class LoginModal extends React.Component<ILoginModalProps, ICaptchaState> implem
     const { captchaError } = this.state;
 
     return (
-      <Modal isOpen={this.props.showModal} toggle={handleClose} backdrop="static" id="login-page" autoFocus={false}>
-        <AvForm onSubmit={this.handleSubmit}>
-          <ModalHeader id="login-title" toggle={handleClose}>
-            <Translate contentKey="login.title">Sign in</Translate>
-          </ModalHeader>
-          <ModalBody>
-            <Row>
-              <Col md="12">
-                {loginError ? (
-                  <Alert color="danger" id="failed-login">
-                    <Translate contentKey="login.messages.error.authentication">
-                      <strong>Failed to sign in!</strong> Please check your credentials and try again.
-                    </Translate>
-                  </Alert>
-                ) : null}
-                {captchaError ? (
-                  <Alert color="danger" id="failed-login">
-                    {captchaError}
-                  </Alert>
-                ) : null}
-              </Col>
-              <Col md="12">
-                <AvField
-                  name="username"
-                  autoComplete="username"
-                  label={translate('global.form.username')}
-                  placeholder={translate('global.form.username.placeholder')}
-                  required
-                  errorMessage="Username cannot be empty!"
-                  autoFocus
-                />
-                <AvField
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  label={translate('login.form.password')}
-                  placeholder={translate('login.form.password.placeholder')}
-                  required
-                  errorMessage="Password cannot be empty!"
-                />
-                <AvGroup check inline>
-                  <Label className="form-check-label">
-                    <AvInput type="checkbox" name="rememberMe" /> <Translate contentKey="login.form.rememberme">Remember me</Translate>
-                  </Label>
-                </AvGroup>
-              </Col>
-            </Row>
-            <div className="mt-1">&nbsp;</div>
-            <Alert color="warning">
-              <Link className="auth-label" to="/reset/request">
-                <Translate contentKey="login.password.forgot">Did you forget your password?</Translate>
-              </Link>
-            </Alert>
-            <Alert color="warning">
-              <span className="auth-label">
-                <Translate contentKey="global.messages.info.register.noaccount">You don't have an account yet?</Translate>
-              </span>{' '}
-              <Link className="auth-label" to="/register">
-                <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
-              </Link>
-            </Alert>
-          </ModalBody>
-          <ModalFooter>
-            <ButtonPill className="button-pill-secondary" onClick={handleClose}>
-              <Translate contentKey="entity.action.cancel">Cancel</Translate>
-            </ButtonPill>{' '}
-            <ButtonPill className={`button-pill-primary`}>
-              <button type="submit" id="submit-button">
-                <Translate contentKey="login.form.button">Sign in</Translate>
-              </button>
-            </ButtonPill>
-          </ModalFooter>
-          <ReCAPTCHA
-            ref={this.recaptchaRef}
-            sitekey={RECAPTCHA_SITE_KEY}
-            onChange={this.onCaptchaChange}
-            onErrored={this.onCaptchaErrored}
-            size="invisible"
-          />
-        </AvForm>
-      </Modal>
+      <>
+        <Modal isOpen={this.props.showModal} toggle={handleClose} backdrop="static" id="login-page" autoFocus={false}>
+          <AvForm onSubmit={this.handleSubmit}>
+            <ModalHeader id="login-title" toggle={handleClose}>
+              <Translate contentKey="login.title">Sign in</Translate>
+            </ModalHeader>
+            <ModalBody>
+              <Row>
+                <Col md="12">
+                  {loginError ? (
+                    <Alert color="danger" id="failed-login">
+                      <Translate contentKey="login.messages.error.authentication">
+                        <strong>Failed to sign in!</strong> Please check your credentials and try again.
+                      </Translate>
+                    </Alert>
+                  ) : null}
+                  {captchaError ? (
+                    <Alert color="danger" id="failed-login">
+                      {captchaError}
+                    </Alert>
+                  ) : null}
+                </Col>
+                <Col md="12">
+                  <AvField
+                    name="username"
+                    autoComplete="username"
+                    label={translate('global.form.username')}
+                    placeholder={translate('global.form.username.placeholder')}
+                    required
+                    errorMessage="Username cannot be empty!"
+                    autoFocus
+                  />
+                  <AvField
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    label={translate('login.form.password')}
+                    placeholder={translate('login.form.password.placeholder')}
+                    required
+                    errorMessage="Password cannot be empty!"
+                  />
+                  <AvGroup check inline>
+                    <Label className="form-check-label">
+                      <AvInput type="checkbox" name="rememberMe" /> <Translate contentKey="login.form.rememberme">Remember me</Translate>
+                    </Label>
+                  </AvGroup>
+                </Col>
+              </Row>
+              <div className="mt-1">&nbsp;</div>
+              <Alert color="warning">
+                <Link className="auth-label" to="/reset/request">
+                  <Translate contentKey="login.password.forgot">Did you forget your password?</Translate>
+                </Link>
+              </Alert>
+              <Alert color="warning">
+                <span className="auth-label">
+                  <Translate contentKey="global.messages.info.register.noaccount">You don't have an account yet?</Translate>
+                </span>{' '}
+                <Link className="auth-label" to="/register">
+                  <Translate contentKey="global.messages.info.register.link">Register a new account</Translate>
+                </Link>
+              </Alert>
+            </ModalBody>
+            <ModalFooter>
+              <ButtonPill className="button-pill-secondary" onClick={handleClose}>
+                <Translate contentKey="entity.action.cancel">Cancel</Translate>
+              </ButtonPill>{' '}
+              <ButtonPill className={`button-pill-primary`}>
+                <button type="submit" id="submit-button">
+                  <Translate contentKey="login.form.button">Sign in</Translate>
+                </button>
+              </ButtonPill>
+            </ModalFooter>
+          </AvForm>
+        </Modal>
+        <ReCAPTCHA
+          ref={this.recaptchaRef}
+          sitekey={RECAPTCHA_SITE_KEY}
+          onChange={this.onCaptchaChange}
+          onErrored={this.onCaptchaErrored}
+          size="invisible"
+          badge="bottomright"
+        />
+      </>
     );
   }
 }
