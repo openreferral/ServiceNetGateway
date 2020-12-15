3099613f7daadcd3e51f683c424c742079270bd8
Jakub Kondrat <jkondrat@soldevelo.com>
2020-12-15 15:14:05 +0100

    benetech/ServiceNet#1149: Copy default opening hours

--- src/main/webapp/app/modules/provider/record/opening-hours.tsx
+++ src/main/webapp/app/modules/provider/record/opening-hours.tsx
@@ -137,7 +137,7 @@ export class OpeningHours extends React.Component<IOpeningHoursProp, {}> {
         )
         .value();
     }
-    return this.props.defaultOpeningHours || [{}];
+    return [...this.props.defaultOpeningHours] || [{}];
   };
 
   parseDatesClosed = location => {
