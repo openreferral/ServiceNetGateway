015bffbd589a7063f82347fcc3bc3cc92d11837c
Jakub Kondrat <jkondrat@soldevelo.com>
2020-12-22 16:58:03 +0100

    benetech/ServiceNet#1411: Website Links in Record active click ons (#392)

--- src/main/webapp/app/modules/provider/record/single-record-view.tsx
+++ src/main/webapp/app/modules/provider/record/single-record-view.tsx
@@ -562,7 +562,9 @@ class SingleRecordView extends React.Component<ISingleRecordViewProps, ISingleRe
                         <Translate contentKey="record.singleRecordView.orgWebsite" />
                       </b>
                     </h6>
-                    <span className="text-break">{organization.url}</span>
+                    <a className="text-break" target="_blank" href={organization.url}>
+                      {organization.url}
+                    </a>
                   </section>
                 ) : null}
                 {organization.email ? (
