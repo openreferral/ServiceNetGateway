04345a638e6954ccb6037b5b4a05d0205fba0ecf
kdondziak <kdondziak@soldevelo.com>
2021-02-12 16:53:39 +0100

    benetech/ServiceNet#1668: Exit button on claiming modal

--- src/main/webapp/app/modules/provider/record/claim-button.tsx
+++ src/main/webapp/app/modules/provider/record/claim-button.tsx
@@ -9,15 +9,22 @@ import { IRootState } from 'app/shared/reducers';
 import ButtonPill from '../shared/button-pill';
 
 export interface IClaimButtonProp extends StateProps, DispatchProps {
-  record: any;
+  record?: any;
+  orgId?: string;
+  style?: object;
 }
 
 export class ClaimButton extends React.Component<IClaimButtonProp, {}> {
   render() {
-    const { record, claimedRecords } = this.props;
-    if (claimedRecords && claimedRecords.indexOf(record.organization.id) > -1) {
+    const { record, claimedRecords, orgId, style } = this.props;
+    const organizationId = record ? record.organization.id : orgId;
+    if (claimedRecords && claimedRecords.indexOf(organizationId) > -1) {
       return (
-        <ButtonPill className="claim-button button-pill-referred" onClick={() => this.props.unmarkRecordToClaim(record.organization.id)}>
+        <ButtonPill
+          className="claim-button button-pill-referred"
+          style={style}
+          onClick={() => this.props.unmarkRecordToClaim(organizationId)}
+        >
           <b className="d-flex align-items-center justify-content-center h-100">
             <Translate contentKey="recordCard.claimed" />
             &nbsp;
@@ -27,7 +34,7 @@ export class ClaimButton extends React.Component<IClaimButtonProp, {}> {
       );
     } else {
       return (
-        <ButtonPill className="claim-button button-pill-refer" onClick={() => this.props.markRecordToClaim(record.organization.id)}>
+        <ButtonPill className="claim-button button-pill-refer" style={style} onClick={() => this.props.markRecordToClaim(organizationId)}>
           <b className="d-flex align-items-center justify-content-center h-100">
             <Translate contentKey="recordCard.claim" />
           </b>
